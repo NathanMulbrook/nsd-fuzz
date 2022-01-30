a6b51656ce764b47c5012bd3130bd2fd6299478d
Nathan Mulbrook <nathanmulbrook@gmail.com>
2022-01-30 15:39:51 -0600

    Use libfuzzer as a library
    Signed-off-by: Nathan Mulbrook: <nathanmulbrook@gmail.com>

--- build.sh
+++ build.sh
@@ -5,17 +5,20 @@ mkdir -p build/run/etc
 mkdir -p build/run/etc/nsd
 
 
-# git clone https://github.com/NLnetLabs/nsd.git
-#git clone git@github.com:NathanMulbrook/nsd.git
+# git clone git@github.com:NathanMulbrook/nsd.git
 cd nsd
-# git checkout tags/NSD_4_3_9_REL
+# git checkout tags/fuzz2
 aclocal && autoconf && autoheader
+
 cd ../build
 
 CC=clang   ../nsd/configure --prefix=$directory/build/run/ --disable-flto
 make clean
-#clang++ -c ../DataFlow.cpp -fsanitize=dataflow -O2 -o DataFlow.o
-#clang++ -c ../DataFlowCallbacks.cpp -O2 -fPIC -o DataFlowCallbacks.o
+cp ../nsd/fuzzer.c ./
+cp ../nsd/fuzzer.h ./
+rm -p fuzzer.o
+clang -c fuzzer.c -pthread -fsanitize=fuzzer-no-link -O2 -o fuzzer.o 
+
 make nsd -j 34
 cp ../nsd.conf run/etc/nsd/nsd.conf
 cp ../example.com.zone run/etc/nsd/example.com.zone
