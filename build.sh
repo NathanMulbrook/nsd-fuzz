2811955b7cdb2884813255120902fc71cefacf6c
Nathan Mulbrook <nathanmulbrook@gmail.com>
2022-02-17 23:23:04 -0600

    Update fuzzer
    Signed-off-by: Nathan Mulbrook: <nathanmulbrook@gmail.com>

--- build.sh
+++ build.sh
@@ -12,13 +12,13 @@ aclocal && autoconf && autoheader
 
 cd ../build
 
-CC=clang   ../nsd/configure --prefix=$directory/build/run/ --disable-flto
+CC=clang CXX=clang++ CFLAGS='-g -O2 -fsanitize=fuzzer-no-link,address,undefined,leak -fno-omit-frame-pointer -fno-optimize-sibling-calls -fprofile-instr-generate -fcoverage-mapping -fsanitize-coverage=trace-cmp -fno-common -fsanitize-address-use-after-scope -fsanitize-address-use-after-return=runtime -U_FORTIFY_SOURCE -D_FORTIFY_SOURCE=0' CXXFLAGS='-g -O2 -fsanitize=fuzzer-no-link,address,undefined,leak -fno-omit-frame-pointer -fno-optimize-sibling-calls -fprofile-instr-generate -fcoverage-mapping -fsanitize-coverage=trace-cmp -fno-common -fsanitize-address-use-after-scope -fsanitize-address-use-after-return=runtime -U_FORTIFY_SOURCE -D_FORTIFY_SOURCE=0'  ../nsd/configure --prefix=$directory/build/run/ --disable-flto --enable-root-server --with-ssl=yes --with-libevent=yes --enable-dnstap 
 make clean
 cp ../nsd/fuzzer.c ./
 cp ../nsd/fuzzer.h ./
 rm -p fuzzer.o
-clang -c fuzzer.c -pthread -fsanitize=fuzzer-no-link -O2 -o fuzzer.o 
+clang -c fuzzer.c -pthread -fsanitize=fuzzer-no-link -Ofast -march=native -o fuzzer.o 
 
-make nsd -j 34
+make install  -j 34
 cp ../nsd.conf run/etc/nsd/nsd.conf
-cp ../example.com.zone run/etc/nsd/example.com.zone
+cp ../example.com.zone run/etc/nsd/*.zone
---
+++ run.sh
@@ -0,0 +1,1 @@
+ASAN_OPTIONS=strict_string_checks=1:detect_stack_use_after_return=1:check_initialization_order=1:strict_init_order=1 ./build/nsd 
