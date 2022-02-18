2ec2afdd84f2cf0672692c91718d566db2856080
Nathan Mulbrook <nathanmulbrook@gmail.com>
2022-02-18 12:28:30 -0600

    Update fuzzer
    Signed-off-by: Nathan Mulbrook: <nathanmulbrook@gmail.com>

--- build.sh
+++ build.sh
@@ -12,7 +12,7 @@ aclocal && autoconf && autoheader
 
 cd ../build
 
-CC=clang CXX=clang++ CFLAGS='-g -O2 -fsanitize=fuzzer-no-link,address,undefined,leak -fno-omit-frame-pointer -fno-optimize-sibling-calls -fprofile-instr-generate -fcoverage-mapping -fsanitize-coverage=trace-cmp -fno-common -fsanitize-address-use-after-scope -fsanitize-address-use-after-return=runtime -U_FORTIFY_SOURCE -D_FORTIFY_SOURCE=0' CXXFLAGS='-g -O2 -fsanitize=fuzzer-no-link,address,undefined,leak -fno-omit-frame-pointer -fno-optimize-sibling-calls -fprofile-instr-generate -fcoverage-mapping -fsanitize-coverage=trace-cmp -fno-common -fsanitize-address-use-after-scope -fsanitize-address-use-after-return=runtime -U_FORTIFY_SOURCE -D_FORTIFY_SOURCE=0'  ../nsd/configure --prefix=$directory/build/run/ --disable-flto --enable-root-server --with-ssl=yes --with-libevent=yes --enable-dnstap 
+CC=clang CXX=clang++ CFLAGS='-g -O2 -fsanitize=fuzzer-no-link,address,undefined,leak -fno-omit-frame-pointer -fno-optimize-sibling-calls -fprofile-instr-generate -fcoverage-mapping -fsanitize-coverage=trace-cmp -fno-common -fsanitize-address-use-after-scope -fsanitize-address-use-after-return=runtime -U_FORTIFY_SOURCE -D_FORTIFY_SOURCE=0' CXXFLAGS='-g -O2 -fsanitize=fuzzer-no-link,address,undefined,leak -fno-omit-frame-pointer -fno-optimize-sibling-calls -fprofile-instr-generate -fcoverage-mapping -fsanitize-coverage=trace-cmp -fno-common -fsanitize-address-use-after-scope -fsanitize-address-use-after-return=runtime -U_FORTIFY_SOURCE -D_FORTIFY_SOURCE=0'  ../nsd/configure --prefix=$directory/build/run/ --disable-flto --enable-root-server --with-ssl=yes $@
 make clean
 cp ../nsd/fuzzer.c ./
 cp ../nsd/fuzzer.h ./
@@ -23,5 +23,5 @@ make install  -j 34
 cp ../nsd.conf run/etc/nsd/nsd.conf
 cp ../*.zone run/etc/nsd/
 ./run/sbin/nsd-control-setup
-mkdir -p run/sbin/corpus
+mkdir -p run/etc/nsd/corpus
 cp ../dict.txt run/etc/nsd/dict.txt
--- run.sh
+++ run.sh
@@ -1,1 +1,2 @@
-ASAN_OPTIONS=strict_string_checks=1:detect_stack_use_after_return=1:check_initialization_order=1:strict_init_order=1 ./build/nsd 
+ASAN_OPTIONS=strict_string_checks=1:detect_stack_use_after_return=1:check_initialization_order=1:strict_init_order=1 ./build/run/sbin/nsd $@
+
